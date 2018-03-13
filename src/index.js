7f4573d96f9f7d8936de2f0237c99ed06e2d6aeb
Sergej Miroshnikov <SVM1983@tut.by>
2018-03-13 19:59:53 +0200

    smart-calculator/feature: implement 285 tasks

--- src/index.js
+++ src/index.js
@@ -1,33 +1,76 @@
 class SmartCalculator {
   constructor(initialValue) {
-    this.value = initialValue;
+	this.sequence  = '(' + initialValue + ')';
+	let sequence = this.sequence;
+	let result, mul, items, pow_count;
 	
-	String.prototype.add = function(a){return  new String(Number(this)+a)}
-	String.prototype.subtract = function(a){return  new String(Number(this)-a)}
-	String.prototype.multiply = function(a){return  new String(Number(this)*a)}
-	String.prototype.devide = function(a){return  new String(Number(this)/a)}
-	String.prototype.pow = function(a){return  new String(Math.pow(Number(this),a))}
+	let findResult = function(){
+		items = [];
+		items = sequence;	
+		pow_count = true;
+		while(pow_count){
+			pow_count = false; 
+			mul = items.match(/\d+[)].pow[(]\d+/);							
+			if(mul){
+				let value_1 = Number(mul[0].match(/^\d+/)[0]);
+				let value_2 = Number(mul[0].match(/\d+$/)[0]);
+				items = items.replace(/\d+[)].pow[(]\d+/,String(Math.pow(value_1,value_2)));
+				pow_count = true;
+			}					
+		}
+		items = items.replace(/.add/g,'+');
+		items = items.replace(/.multiply/g,'*');
+		items = items.replace(/.subtract/g,'-');
+		items = items.replace(/.devide/g,'/');
+		result = eval(items);
+		return result;
+	}
+	
+	String.prototype.add = function(a){		
+		sequence = sequence + '.add(' + a + ')';				
+		return  new String(findResult());
+	}
+	
+	String.prototype.subtract = function(a){
+		sequence = sequence + '.subtract(' + a + ')';		
+		return  new String(findResult());
+	}
+
+	String.prototype.multiply = function(a){
+		sequence = sequence + '.multiply(' + a + ')';		
+		return  new String(findResult());
+	}
+	
+	String.prototype.devide = function(a){
+		sequence = sequence + '.devide(' + a + ')';		
+		return  new String(findResult());
+	}
+	
+	String.prototype.pow = function(a){
+		sequence = sequence + '.pow(' + a + ')';		
+		return  new String(findResult());
+	}
   }
 
   add(number) {
-	return new String(this.value+number);
+	return new String(this.sequence.add(number));
   }
   
   subtract(number) {
-	return new String(this.value-number);
+	return new String(this.sequence.subtract(number));
   }
 
   multiply(number) {
-	return new String(this.value*number);
+	return new String(this.sequence.multiply(number));
   }
 
   devide(number) {
-	return new String(this.value/number);
+	return new String(this.sequence.devide(number));
   }
 
   pow(number) {
-	return new String(Math.pow(this.value,number));
+	return new String(this.sequence.pow(number));
   }
 }
 
-module.exports = SmartCalculator;
+ module.exports = SmartCalculator;
